84b3cfcba5cdb5dbb56968453a131397e3c7c0d3
Rafi Ramadhan <rafiisakh@gmail.com>
2025-08-16 21:36:21 +0900

    implement resume analysis functions

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -15,3 +15,5 @@ export function formatSize(bytes: number): string {
   // Format the number with 2 decimal places and return with the unit
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
+
+export const generateUUID = () => crypto.randomUUID()
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,8 +1,15 @@
 import { type FormEvent, useState } from 'react'
 import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import {convertPdfToImage} from "~/lib/pdf2img";
+import {generateUUID} from "~/lib/utils";
+import {prepareInstructions} from "../../constants";
 
 const Upload = () => {
+    const { auth, isLoading, fs, ai, kv } = usePuterStore()
+    const navigate = useNavigate()
     const [isProcessing, setProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
     const [file, setFile] = useState<File | null>(null);
@@ -11,19 +18,71 @@ const Upload = () => {
         setFile(file)
     }
 
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File}) => {
+
+        try {
+            setProcessing(true)
+            setStatusText('Uploading your resume...');
+            const uploadedFile = await fs.upload([file])
+
+            if (!uploadedFile) return setStatusText('Error: Failed to upload file')
+
+            setStatusText('Converting to image...')
+            const imageFile = await convertPdfToImage(file)
+            if (!imageFile.file) return setStatusText('Error: Failed to convert image')
+
+            setStatusText('Uploading the image...')
+            const uploadedImage = await fs.upload([imageFile.file])
+            if (!uploadedImage) return setStatusText('Error: Failed to upload image')
+
+            setStatusText('Preparing data...')
+            const uuid = generateUUID()
+            const data = {
+                id: uuid,
+                resumePath: uploadedFile.path,
+                imagePath: uploadedImage.path,
+                companyName, jobTitle, jobDescription,
+                feedback: '',
+            }
+            await kv.set(`resume:${uuid}`, JSON.stringify(data))
+
+            setStatusText('Analyzing...')
+
+            const feedback = await ai.feedback(
+                uploadedFile.path,
+                prepareInstructions({ jobTitle, jobDescription })
+            )
+            if (!feedback) return setStatusText('Error: Failed to analyze resume')
+
+            const feedbackText = typeof feedback.message.content === 'string'
+                ? feedback.message.content
+                : feedback.message.content[0].text
+
+            data.feedback = JSON.parse(feedbackText)
+            await kv.set(`resume:${uuid}`, JSON.stringify(data))
+            setStatusText('Analysis complete, redirecting...')
+            console.log(data)
+        } catch (error) {
+            console.error('Analyse error: ', error);
+            setStatusText('Error: Failed to analyze resume. Please try again later.');
+        } finally {
+            setProcessing(false)
+        }
+    }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const form = e.currentTarget.closest('form');
         if (!form) return
         const formData = new FormData(form);
 
-        const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
-        const jobDescription = formData.get('job-description');
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
 
-        console.log({
-            companyName, jobTitle, jobDescription, file
-        })
+        if(!file) return;
+
+        handleAnalyze({ companyName, jobTitle, jobDescription, file})
     }
 
     return (
